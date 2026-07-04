994cb87d674a75b6aed8d6be13a99df3024a2bda
Saikumar Bali <saikumarbali55@gmail.com>
2026-07-04 08:21:10 +0530

    phase-8: register store fulfillment handlers

--- apps/api-gateway/src/orders/order.module.ts
+++ apps/api-gateway/src/orders/order.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { StoreFulfillmentController } from './store-fulfillment.controller';
+import { StoreFulfillmentService } from './store-fulfillment.service';
 import { PaymentsModule } from '../payments/payments.module';
 
 @Module({
   imports: [PaymentsModule],
-  controllers: [OrderController],
-  providers: [OrderService],
+  controllers: [
+    OrderController,
+    StoreFulfillmentController,
+  ],
+  providers: [
+    OrderService,
+    StoreFulfillmentService,
+  ],
   exports: [OrderService],
 })
 export class OrderModule {}
