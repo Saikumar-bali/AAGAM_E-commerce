05a9658bfda145b8ccabd04aaf3a3e745bf2f026
Saikumar Bali <saikumarbali55@gmail.com>
2026-07-05 08:01:07 +0530

    phase-9: wire dispatch rider operations module

--- apps/api-gateway/src/orders/order.module.ts
+++ apps/api-gateway/src/orders/order.module.ts
@@ -3,6 +3,8 @@ import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { StoreFulfillmentController } from './store-fulfillment.controller';
 import { StoreFulfillmentService } from './store-fulfillment.service';
+import { DispatchController } from './dispatch.controller';
+import { DispatchService } from './dispatch.service';
 import { PaymentsModule } from '../payments/payments.module';
 
 @Module({
@@ -10,10 +12,12 @@ import { PaymentsModule } from '../payments/payments.module';
   controllers: [
     OrderController,
     StoreFulfillmentController,
+    DispatchController,
   ],
   providers: [
     OrderService,
     StoreFulfillmentService,
+    DispatchService,
   ],
   exports: [OrderService],
 })
