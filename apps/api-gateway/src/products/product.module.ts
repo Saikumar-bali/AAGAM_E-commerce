7175081d44690d3a075784a3031da3c020213acd
Saikumar Bali <saikumarbali55@gmail.com>
2026-07-07 03:37:54 +0530

    feat(products): register admin product controller

--- apps/api-gateway/src/products/product.module.ts
+++ apps/api-gateway/src/products/product.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { AdminProductController } from './admin-product.controller';
 
 @Module({
-  controllers: [ProductController],
+  controllers: [ProductController, AdminProductController],
   providers: [ProductService],
 })
 export class ProductModule {}
