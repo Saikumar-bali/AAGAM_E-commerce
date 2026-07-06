be97f2352d373ed8cb8c7c9b95bef469acdc895f
Saikumar Bali <saikumarbali55@gmail.com>
2026-07-06 09:17:38 +0530

    phase-17b: register Firebase PNV native module

--- apps/mobile-customer/android/app/src/main/java/com/aagamcustomer/MainApplication.kt
+++ apps/mobile-customer/android/app/src/main/java/com/aagamcustomer/MainApplication.kt
@@ -4,14 +4,27 @@ import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
-import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
+import com.facebook.react.uimanager.ViewManager
+
+class CustomerNativePackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): MutableList<NativeModule> {
+    return mutableListOf(FirebasePnvModule(reactContext))
+  }
+  override fun createViewManagers(reactContext: ReactApplicationContext): MutableList<ViewManager<*, *>> {
+    return mutableListOf()
+  }
+}
 
 class MainApplication : Application(), ReactApplication {
   override val reactHost: ReactHost by lazy {
     getDefaultReactHost(
       context = applicationContext,
-      packageList = PackageList(this).packages.apply {},
+      packageList = PackageList(this).packages.apply { add(CustomerNativePackage()) },
     )
   }
   override fun onCreate() {
