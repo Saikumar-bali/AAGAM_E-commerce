aeec5676dc72448c2a23c67acc94e24645ac100b
Saikumar Bali <saikumarbali55@gmail.com>
2026-07-12 14:15:29 +0530

    feat(notifications): register operational routing overrides

--- apps/api-gateway/src/notifications/notifications.module.ts
+++ apps/api-gateway/src/notifications/notifications.module.ts
@@ -1,6 +1,7 @@
 import { Global, Module } from '@nestjs/common';
 import { NotificationDeliveryService } from './notification-delivery.service';
 import { NotificationRoutingService } from './notification-routing.service';
+import { OperationalNotificationRoutingService } from './operational-notification-routing.service';
 import { NotificationService } from './notification.service';
 import { NotificationWorkerService } from './notification-worker.service';
 import { NotificationsController } from './notifications.controller';
@@ -15,7 +16,8 @@ import { WebPushService } from './web-push.service';
     WebPushService,
     PushSubscriptionService,
     OutboxService,
-    NotificationRoutingService,
+    OperationalNotificationRoutingService,
+    { provide: NotificationRoutingService, useExisting: OperationalNotificationRoutingService },
     NotificationDeliveryService,
     NotificationService,
     NotificationWorkerService,
