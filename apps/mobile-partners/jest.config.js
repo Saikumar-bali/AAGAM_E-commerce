15fbcf5a6dd2fe9cd1d4416370572b46bf294ddc
Saikumar Bali <saikumarbali55@gmail.com>
2026-07-23 16:33:29 +0530

    fix: use repository-compatible Jest configuration

--- apps/mobile-partners/jest.config.js
+++ apps/mobile-partners/jest.config.js
@@ -1,15 +1,7 @@
-const path = require('path');
-
-const localReact = path.resolve(__dirname, 'node_modules/react');
-
 module.exports = {
   preset: '@react-native/jest-preset',
   testMatch: ['<rootDir>/src/**/*.spec.ts'],
   testPathIgnorePatterns: ['/node_modules/'],
   clearMocks: true,
   restoreMocks: true,
-  moduleNameMapper: {
-    '^react$': localReact,
-    '^react/(.*)$': path.resolve(__dirname, 'node_modules/react/$1'),
-  },
 };
