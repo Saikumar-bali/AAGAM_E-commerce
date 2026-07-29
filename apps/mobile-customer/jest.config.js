f9527b452c5c04589b2a1d930509e2f6dac941eb
Saikumar Bali <saikumarbali55@gmail.com>
2026-07-29 18:00:25 +0530

    use installed react native jest preset

--- apps/mobile-customer/jest.config.js
+++ apps/mobile-customer/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: '@react-native/jest-preset',
+  preset: 'react-native',
   testMatch: ['<rootDir>/src/**/*.spec.ts'],
   testPathIgnorePatterns: ['/node_modules/'],
   clearMocks: true,
