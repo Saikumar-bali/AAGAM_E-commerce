6757c9d14655ad213516a61eaa816e6baacb3dcd
Saikumar Bali <saikumarbali55@gmail.com>
2026-07-29 18:04:42 +0530

    remove unused customer jest config

--- apps/mobile-customer/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'react-native',
-  testMatch: ['<rootDir>/src/**/*.spec.ts'],
-  testPathIgnorePatterns: ['/node_modules/'],
-  clearMocks: true,
-  restoreMocks: true,
-};
