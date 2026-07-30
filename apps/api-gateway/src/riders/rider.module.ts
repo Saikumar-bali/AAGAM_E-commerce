ece94d6f27c5a5ac424822db46fc5383dd4dec44
Saikumar Bali <saikumarbali55@gmail.com>
2026-07-30 07:04:01 +0530

    harden automatic rider dispatch recovery

--- apps/api-gateway/src/riders/rider.module.ts
+++ apps/api-gateway/src/riders/rider.module.ts
@@ -1,11 +1,13 @@
-import { Module } from "@nestjs/common";
-import { RiderService } from "./rider.service";
-import { RiderController } from "./rider.controller";
-import { RiderPortalController } from "./rider-portal.controller";
-import { RiderPortalService } from "./rider-portal.service";
-import { RiderAdminController } from "./rider-admin.controller";
+import { Module } from '@nestjs/common';
+import { OrderModule } from '../orders/order.module';
+import { RiderAdminController } from './rider-admin.controller';
+import { RiderController } from './rider.controller';
+import { RiderPortalController } from './rider-portal.controller';
+import { RiderPortalService } from './rider-portal.service';
+import { RiderService } from './rider.service';
 
 @Module({
+  imports: [OrderModule],
   controllers: [RiderController, RiderPortalController, RiderAdminController],
   providers: [RiderService, RiderPortalService],
 })
