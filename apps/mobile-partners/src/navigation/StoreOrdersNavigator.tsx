40fd5a8fb7cbe725833c0937c6774190dfe9555e
Saikumar Bali <saikumarbali55@gmail.com>
2026-08-02 20:57:10 +0530

    Route store orders through reference details screen

--- apps/mobile-partners/src/navigation/StoreOrdersNavigator.tsx
+++ apps/mobile-partners/src/navigation/StoreOrdersNavigator.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StoreOrdersScreen } from '../screens/store/StoreOrdersScreen';
 import { StoreOrderDetailsScreen } from '../screens/store/StoreOrderDetailsScreen';
+import { StoreOrderDetailsReferenceScreen } from '../screens/store/StoreOrderDetailsReferenceScreen';
 
 const Stack = createNativeStackNavigator();
 
 export const StoreOrdersNavigator = () => (
   <Stack.Navigator screenOptions={{ headerShown: false }}>
     <Stack.Screen name="OrderQueue" component={StoreOrdersScreen} />
-    <Stack.Screen name="OrderDetails" component={StoreOrderDetailsScreen} />
+    <Stack.Screen name="OrderDetails" component={StoreOrderDetailsReferenceScreen} />
+    <Stack.Screen name="AdvancedOrderDetails" component={StoreOrderDetailsScreen} />
   </Stack.Navigator>
 );
