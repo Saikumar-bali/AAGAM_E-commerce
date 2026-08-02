da0373e42653043a577a3a5e47d6eca169a589bf
Saikumar Bali <saikumarbali55@gmail.com>
2026-08-02 21:02:15 +0530

    Resolve pickup verification navigation from tabs or stack

--- apps/mobile-partners/src/screens/store/StorePickupVerificationEntryScreen.tsx
+++ apps/mobile-partners/src/screens/store/StorePickupVerificationEntryScreen.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { StorePickupVerificationScreen } from './StorePickupVerificationScreen';
 
-export const StorePickupVerificationEntryScreen = (props: any) => (
-  <StorePickupVerificationScreen
-    {...props}
-    route={{
-      ...props.route,
-      params: {
-        ...(props.route?.params || {}),
-        deliveryJobId: props.route?.params?.deliveryJobId || '__FIRST_WAITING__',
-      },
-    }}
-  />
-);
+export const StorePickupVerificationEntryScreen = (props: any) => {
+  const stackNavigation = typeof props.navigation?.replace === 'function'
+    ? props.navigation
+    : props.navigation?.getParent?.() || props.navigation;
+
+  return (
+    <StorePickupVerificationScreen
+      {...props}
+      navigation={stackNavigation}
+      route={{
+        ...props.route,
+        params: {
+          ...(props.route?.params || {}),
+          deliveryJobId: props.route?.params?.deliveryJobId || '__FIRST_WAITING__',
+        },
+      }}
+    />
+  );
+};
