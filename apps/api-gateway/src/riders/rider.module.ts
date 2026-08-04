b26509ae6695491a39a63147ebd22dba0306d4c8
Saikumar Bali <saikumarbali55@gmail.com>
2026-08-04 08:19:56 +0530

    Revert unfinished Rider platform module wiring

--- apps/api-gateway/src/riders/rider.module.ts
+++ apps/api-gateway/src/riders/rider.module.ts
@@ -1,16 +1,14 @@
 import { Module } from '@nestjs/common';
 import { OrderModule } from '../orders/order.module';
-import { UploadModule } from '../upload/upload.module';
 import { RiderAdminController } from './rider-admin.controller';
 import { RiderController } from './rider.controller';
 import { RiderPortalController } from './rider-portal.controller';
-import { RiderPlatformService } from './rider-platform.service';
 import { RiderPortalService } from './rider-portal.service';
 import { RiderService } from './rider.service';
 
 @Module({
-  imports: [OrderModule, UploadModule],
+  imports: [OrderModule],
   controllers: [RiderController, RiderPortalController, RiderAdminController],
-  providers: [RiderService, RiderPortalService, RiderPlatformService],
+  providers: [RiderService, RiderPortalService],
 })
 export class RiderModule {}
