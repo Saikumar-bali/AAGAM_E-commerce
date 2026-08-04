1a00b6ca9ec3665ee945994db08a607bfcef673d
Saikumar Bali <saikumarbali55@gmail.com>
2026-08-04 21:30:37 +0530

    feat(rider): register portal read services

--- apps/api-gateway/src/riders/rider.module.ts
+++ apps/api-gateway/src/riders/rider.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { OrderModule } from '../orders/order.module';
+import { UploadModule } from '../upload/upload.module';
 import { RiderAdminController } from './rider-admin.controller';
 import { RiderController } from './rider.controller';
 import { RiderPortalController } from './rider-portal.controller';
+import { RiderPortalReadService } from './rider-portal-read.service';
 import { RiderPortalService } from './rider-portal.service';
 import { RiderService } from './rider.service';
 
 @Module({
-  imports: [OrderModule],
+  imports: [OrderModule, UploadModule],
   controllers: [RiderController, RiderPortalController, RiderAdminController],
-  providers: [RiderService, RiderPortalService],
+  providers: [RiderService, RiderPortalService, RiderPortalReadService],
 })
 export class RiderModule {}
