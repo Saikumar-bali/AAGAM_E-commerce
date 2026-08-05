e052e3fcaee60ff8e64caf92b088c7f32d1f69dd
Saikumar Bali <saikumarbali55@gmail.com>
2026-08-05 17:13:46 +0530

    test(partners): execute JavaScript UI audit specs

--- apps/mobile-partners/jest.config.js
+++ apps/mobile-partners/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: '@react-native/jest-preset',
-  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  testMatch: ['<rootDir>/src/**/*.spec.ts', '<rootDir>/src/**/*.spec.js'],
   testPathIgnorePatterns: ['/node_modules/'],
   clearMocks: true,
   restoreMocks: true,
