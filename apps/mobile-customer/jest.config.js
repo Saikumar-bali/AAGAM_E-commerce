9089c2b1d057d4ec617968f2345f7da1190da65b
Saikumar Bali <saikumarbali55@gmail.com>
2026-08-05 17:13:38 +0530

    test(customer): execute JavaScript UI audit specs

--- apps/mobile-customer/jest.config.js
+++ apps/mobile-customer/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  testMatch: ['<rootDir>/src/**/*.spec.ts', '<rootDir>/src/**/*.spec.js'],
   testPathIgnorePatterns: ['/node_modules/'],
   transform: {
     '^.+\\.(ts|tsx)$': '<rootDir>/scripts/jest-typescript-transformer.js',
